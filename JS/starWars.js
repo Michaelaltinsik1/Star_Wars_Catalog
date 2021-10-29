fdef9c1ac030b432be1e92c5beda5d41ac60eca9
Michaelalt1 <49063600+Michaelalt1@users.noreply.github.com>
2021-10-29 13:50:02 +0200

    fixed planets details and loader on both boxes
    Co-authored-by: Artur Lindström <ArturLindstrom@users.noreply.github.com>

--- JS/starWars.js
+++ JS/starWars.js
@@ -1,18 +1,20 @@
-let test = document.querySelector(".test");
-    test.addEventListener("click", () => {
-        main();
-});  
+// let test = document.querySelector(".test");
+//     test.addEventListener("click", () => {
+//         main();
+// });  
 
 function main(){
     
         getFirstPage();
         getPages();   
 }
+main()
 
 function getFirstPage(){
     const request = fetch("https://swapi.dev/api/people/?page=1");
     let characters = document.querySelector(".characters")
     request.then(response => response.json()).then(data =>{
+        document.querySelector(".loader-characters").classList.add("removeloader")
         for(let character of data.results){
             let button = document.createElement("button");
             button.innerText = character.name;
@@ -30,11 +32,13 @@ function getPages(){
    
     previousPage.addEventListener("click", () => {   
         if(currentPage > 1){
+            document.querySelector(".loader-characters").classList.remove("removeloader");
             document.querySelector(".current-page").innerText = --currentPage;
             const request = fetch("https://swapi.dev/api/people/?page="+ currentPage);
             let characters = document.querySelector(".characters")
             characters.innerHTML = "";
             request.then(response => response.json()).then(data =>{
+                document.querySelector(".loader-characters").classList.add("removeloader")
             for(let character of data.results){
                 let button = document.createElement("button");
                 button.innerText = character.name;
@@ -49,11 +53,13 @@ function getPages(){
     });
     nextPage.addEventListener("click", () =>{
         if(currentPage < 9){
+            document.querySelector(".loader-characters").classList.remove("removeloader");
             document.querySelector(".current-page").innerText = ++currentPage;
             const request = fetch("https://swapi.dev/api/people/?page="+ currentPage);
             let characters = document.querySelector(".characters")
             characters.innerHTML = "";
             request.then(response => response.json()).then(data =>{
+                document.querySelector(".loader-characters").classList.add("removeloader")
             for(let character of data.results){
                 let button = document.createElement("button");
                 button.innerText = character.name;
@@ -70,8 +76,10 @@ function getPages(){
 function displayCharacters(characters){
     let buttons = document.querySelectorAll(".characters button");
     let details = document.querySelector(".person-details");
+    let planetDetails = document.querySelector(".planet-details");
     for(let button of buttons){
-        button.addEventListener("click", () =>{                     
+        button.addEventListener("click", () =>{ 
+
             for(let character of characters){
                 if(character.name === button.innerText){
                     details.innerHTML = `
@@ -84,7 +92,21 @@ function displayCharacters(characters){
                         <p>Birth year: ${character.birth_year}</p>
                         <p>Gender: ${character.gender}</p>                   
                     `
+                    planetDetails.innerHTML = `<div class="loader-planets loadplanets"></div>`;
+                    let loadplanets = document.querySelector(".loader-planets");
                     
+                    let request = fetch(character.homeworld);
+                    request.then(response => response.json()).then(data =>{
+                        planetDetails.innerHTML = `
+                        <h3>${data.name}</h3>
+                        <p>Rotation period: ${data.rotation_period}</p>
+                        <p>Orbital period: ${data.orbital_period}</p>
+                        <p>Diameter: ${data.diameter}</p>
+                        <p>Climate: ${data.climate}</p>
+                        <p>Gravity: ${data.gravity}</p>
+                        <p>Terrain: ${data.terrain}</p>                
+                    `
+                    });
                 }
             }
         });
